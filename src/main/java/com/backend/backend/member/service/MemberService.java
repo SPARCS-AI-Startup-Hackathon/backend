fe4ba31ccb5c874577d37f999dc8efc0924757a6
정우혁 <alalalalalalall367@gmail.com>
2024-07-29 20:12:20 +0900

    feat: MemberService 내부 회원가입 서비스 로직 설계

---
+++ src/main/java/com/backend/backend/member/service/MemberService.java
@@ -0,0 +1,30 @@
+package com.backend.backend.member.service;
+
+import com.backend.backend.member.domain.Member;
+import com.backend.backend.member.dto.request.MemberRequest;
+import com.backend.backend.member.exception.MemberDuplicate;
+import com.backend.backend.member.mapper.MemberMapper;
+import com.backend.backend.member.repository.MemberRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+    private final MemberMapper memberMapper;
+
+    @Transactional
+    public Long signIn(MemberRequest request) {
+        if (memberRepository.findOneWithAuthoritiesByEmail(request.getEmail()).orElse(null) != null) {
+            throw new MemberDuplicate();
+        }
+
+        Member member = memberMapper.toEntity(request);
+        memberRepository.save(member);
+
+        return member.getId();
+    }
+}
