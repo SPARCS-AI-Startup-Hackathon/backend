c5fdd11b1b162bf89ef7360c0c42d36876c73df4
정우혁 <alalalalalalall367@gmail.com>
2024-07-30 23:22:40 +0900

    feat: ClovaMapper 내부 첫번째 질문 프롬프팅을 위한 firstQuestion 메서드 설계

--- src/main/java/com/backend/backend/ai/mapper/ClovaMapper.java
+++ src/main/java/com/backend/backend/ai/mapper/ClovaMapper.java
@@ -3,24 +3,51 @@ package com.backend.backend.ai.mapper;
 import com.backend.backend.ai.dto.request.ChatMessage;
 import com.backend.backend.ai.dto.request.ClovaRequest;
 import com.backend.backend.ai.dto.request.ClovaRequestList;
+import com.backend.backend.global.ai.Prompt;
+import com.backend.backend.member.domain.Member;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Component
+@RequiredArgsConstructor
 public class ClovaMapper {
+    private final RedisTemplate<String, String> redisTemplate;
 
-    // 사용자의 응답 메시지 파싱
-    public ClovaRequestList messageMapper(ChatMessage request) {
+    // 첫번째 질문 파싱
+    public ClovaRequestList firstQuestion(Member member) {
         List<ClovaRequest> requestList = new ArrayList<>();
 
-        ClovaRequest message = ClovaRequest.builder()
+        ClovaRequest clovaRequest = ClovaRequest.builder()
+                .role("system")
+                .content(Prompt.PROMPT_DATA + "나이:" + member.getAge() + "이름:" + member.getName()
+                        + member.getName() + "님의 특징:" + member.getIntroduction())
+                .build();
+
+        requestList.add(clovaRequest);
+
+        return ClovaRequestList.builder()
+                .messages(requestList)
+                .build();
+    }
+
+    public ClovaRequestList messageMapper(ChatMessage request, Member member) {
+        List<ClovaRequest> requestList = new ArrayList<>();
+//        // 현재까지의 질문 및 답변을 넣어줘야 함.
+//        ClovaRequest.builder()
+//                .role("system")
+//                .content(QUESTIONS + )
+//                .build();
+
+        ClovaRequest userMessage = ClovaRequest.builder()
                 .role("user")
                 .content(request.getMessage())
                 .build();
 
-        requestList.add(message);
+        requestList.add(userMessage);
 
         return ClovaRequestList.builder()
                 .messages(requestList)
