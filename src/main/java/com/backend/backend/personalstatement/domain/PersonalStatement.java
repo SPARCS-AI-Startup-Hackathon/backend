22ae7f13e0a466580aebb84a095747b5639afa66
정우혁 <alalalalalalall367@gmail.com>
2024-07-31 21:38:26 +0900

    feat : 자기소개서 엔티티 설계

---
+++ src/main/java/com/backend/backend/personalstatement/domain/PersonalStatement.java
@@ -0,0 +1,34 @@
+package com.backend.backend.personalstatement.domain;
+
+import com.backend.backend.member.domain.Member;
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class PersonalStatement {
+    @Id
+    @Column(name = "personal_statement_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    @Lob
+    @Column(columnDefinition = "LONGTEXT")
+    private String content;
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private Member member;
+
+    public void update(String content) {
+        this.content = content;
+    }
+    @Builder
+    public PersonalStatement(String name, String content, Member member) {
+        this.name = name;
+        this.content = content;
+        this.member = member;
+    }
+}
