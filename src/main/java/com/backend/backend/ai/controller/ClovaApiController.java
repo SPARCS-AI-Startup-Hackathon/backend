88bafa53eade861421a6bc3fd215a819f14f5218
정우혁 <alalalalalalall367@gmail.com>
2024-07-31 13:39:36 +0900

    feat: ClovaApiController 내부 API 설계

--- src/main/java/com/backend/backend/ai/controller/ClovaApiController.java
+++ src/main/java/com/backend/backend/ai/controller/ClovaApiController.java
@@ -1,13 +1,17 @@
 package com.backend.backend.ai.controller;
 
 import com.backend.backend.ai.dto.request.ChatMessage;
+import com.backend.backend.ai.dto.response.ChatList;
+import com.backend.backend.ai.dto.response.SttResponse;
 import com.backend.backend.ai.service.ClovaApiService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import reactor.core.publisher.Flux;
 
 
@@ -23,9 +27,34 @@ public class ClovaApiController {
         return clovaApiService.firstSetting(token);
     }
     @PostMapping("/first-question-answer")
-    @Operation(summary = "사용자 첫 답변 회수", description = "사용자의 첫 답변을 추출하는 API")
+    @Operation(summary = "사용자의 답변", description = "사용자가 질문에 대한 답변을 한다")
     public void firstAnswer(@RequestBody @Valid ChatMessage message){
         clovaApiService.getFirstAnswer(message);
+    }
+    @GetMapping(value = "/question/{token}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    @Operation(summary = "질문 생성", description = "질문과 사용자의 답변을 기반으로 질문을 생성해주는 API")
+    public Flux<String> buildQuestion(@PathVariable String token) throws Exception {
+        return clovaApiService.callChatCompletionApi(token);
+    }
+    @PostMapping("/sound-to-text")
+    @Operation(summary = "음성녹음 분석", description = "음성녹음 분석 API")
+    public ResponseEntity<SttResponse> getTextByFile(@RequestParam("file") MultipartFile file) {
+        return ResponseEntity.ok(clovaApiService.getTextByFile(file));
+    }
+    @GetMapping("/get-chat")
+    @Operation(summary = "채팅 내역 보기", description = "AI와 주고받은 채팅내역을 조회한다")
+    public ResponseEntity<ChatList> getChatHistory() {
+        return ResponseEntity.ok(clovaApiService.getChatHistory());
+    }
+    @GetMapping("/recommend/{token}")
+    @Operation(summary = "직업 추천 받기", description = "사용자가 지금까지 했던 대화내역을 기반으로 직업을 추천받는다")
+    public Flux<String> getRecommend(@PathVariable String token) throws Exception {
+        return clovaApiService.getRecommend(token);
+    }
+    @GetMapping("/re-recommend/{token}")
+    @Operation(summary = "직업 재추천 받기", description = "직업이 마음에 들지 않을시 재추천 받는다.")
+    public void getReRecommend(@PathVariable String token) throws Exception {
+        // TODO 서비스 로직 구현
     }
 }
 
